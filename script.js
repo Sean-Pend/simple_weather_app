277b6e3c9d92d6c78129c0ee16467051c095ab36
Sean-Pend <pendgaming1@gmail.com>
2025-12-15 16:20:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Selecting DOM elements
 const searchBtn = document.querySelector('#searchBtn');
 const cityInput = document.querySelector('#cityInput');
-
+// UI output targets
 const locationEl = document.querySelector('#location');
 const temperatureEl = document.querySelector('#temperature');
 const conditionEl = document.querySelector('#condition');
 const errorEl = document.querySelector('#error');
 
+// event handler
 searchBtn.addEventListener('click', getWeather);
 
 async function getWeather() {
+    // reads user input
   const city = cityInput.value.trim();
   errorEl.textContent = '';
 
@@ -18,34 +21,42 @@ async function getWeather() {
   }
 
   try {
-    // 1️⃣ Get latitude & longitude from city name
+    // 1️. Get latitude & longitude from city name
+    // convert city name to coordinates
     const geoResponse = await fetch(
       `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
     );
+    // parse data to JSON
     const geoData = await geoResponse.json();
-
+    
+    // handles invalid city names
     if (!geoData.results) {
       errorEl.textContent = 'City not found';
       return;
     }
 
+    // destructuring
     const { latitude, longitude, name, country } = geoData.results[0];
 
-    // 2️⃣ Get weather using coordinates
+    // 2️. Get weather using coordinates
     const weatherResponse = await fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
     );
+    // parse data to JSON
     const weatherData = await weatherResponse.json();
 
     const temp = weatherData.current_weather.temperature;
     const code = weatherData.current_weather.weathercode;
 
-    // 3️⃣ Update UI
+    // 3️. Update UI
     locationEl.textContent = `${name}, ${country}`;
+    // displays temperature
     temperatureEl.textContent = `Temperature: ${temp}°C`;
+    // displays weather condition
     conditionEl.textContent = `Weather code: ${code}`;
 
   } catch (error) {
+    // handles errors and network failures
     errorEl.textContent = 'Something went wrong';
     console.error(error);
   }
